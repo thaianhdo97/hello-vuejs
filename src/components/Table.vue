b3ac253cff240c07b08f0a5ca0fc5f7181b8f1f5
thaianh97 <58674701+thaianh97@users.noreply.github.com>
2021-06-19 22:09:14 +0700

    update project

---
+++ src/components/Table.vue
@@ -0,0 +1,41 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th v-for="col in columns">{{ col.label }}</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in data">
+      <td v-for="col in columns">
+        <template v-if="col.field !== 'action'">
+          {{ item[col.field] }}
+        </template>
+        <template v-else>
+          <a href="javascript:void(0)">Edit</a> | <a href="javascript:void(0)">Delete</a>
+        </template>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    columns: {
+      type: Array,
+      default: () => []
+    }
+  },
+  name: "Table"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Student.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    {{ titleConverted }} <br>
+    <Table :columns="columns" :data="students"/>
+  </div>
+</template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  name: "Student",
+  components: {
+    Table
+  },
+  data() {
+    return {
+      title: 'student management',
+      students: [],
+      columns: [
+        {
+          label: 'Id',
+          field: 'id',
+        },
+        {
+          label: 'Full Name',
+          field: 'fullName',
+        },
+        {
+          label: 'Email',
+          field: 'email',
+        },
+        {
+          label: 'Status',
+          field: 'status',
+        },
+        {
+          label: 'Created At',
+          field: 'createdAt',
+        },
+        {
+          label: 'Action',
+          field: 'action',
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.loadStudent()
+  },
+  methods: {
+    loadStudent() {
+      fetch('http://localhost:8081/api/students')
+          .then(response => response.json())
+          .then(data => this.students = data);
+    }
+  },
+  computed: {
+    titleConverted() {
+      return this.title.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
